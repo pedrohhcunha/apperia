04ca207e64d37a586559d098806b6af5af26c5a9
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-04 21:21:12 -0300

    Add back to top button

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function toTop() {
+    console.log("toTop")
+    window.scrollTo(0, 0)
+}
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+        document.querySelector('#toTop').classList.toggle('active')
+    } else {
+        document.querySelector('#toTop').classList.toggle('active')
+    }
+})
